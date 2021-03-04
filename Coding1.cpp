8b0627956dc6756739cfc190ed16b6abc08aac71
alaska2007 <alaskanapitupulu123@gmail.com>
2021-03-04 16:10:32 +0700

    coding dasar

---
+++ Coding1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int angka;
+
+	cout << "Menentukan angka yang diinput Positif, Negatif, atau 0" << endl;
+	cout << "------------------------------------------------------" << endl;
+	cout << "Masukkan angka = ";
+	cin >> angka;
+
+	cout << endl;
+
+	if(angka > 0)
+	{
+		cout << "Angka yang ada masukkan adalah POSITIF" << endl;
+	} 
+	else if(angka < 0)
+	{
+		cout << "Angka yang ada masukkan adalah NEGATIF" << endl;
+	}
+	else if(angka = 0)
+	{
+		cout << "Angka yang ada masukkan adalah 0" << endl;
+	}
+	else
+	{
+		cout << "Yang anda input bukan angka" << endl;
+	}
+
+	return 0;
+}
+
